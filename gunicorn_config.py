2d9c5916e36790839cef8fd410172d9570b7550e
PanchoIntegral <yacckoc4@gmail.com>
2025-05-25 02:39:45 +0000

    Desplegando Sitema

---
+++ gunicorn_config.py
@@ -0,0 +1,17 @@
+bind = "127.0.0.1:5000"
+workers = 3
+worker_class = "sync"
+worker_connections = 1000
+timeout = 30
+keepalive = 2
+max_requests = 1000
+max_requests_jitter = 100
+preload_app = True
+reload = False
+daemon = False
+user = "root"
+group = "root"
+logfile = "/var/log/gunicorn/sistema_consul.log"
+loglevel = "info"
+access_logfile = "/var/log/gunicorn/sistema_consul_access.log"
+access_logformat = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
